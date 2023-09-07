b54275507ec2e04b09ffbc32fcd97aadb53e2b0e
Ефремов Кирилл <kefremov@htc-cs.ru>
2023-09-07 12:49:58 +0400

    feat(LinearAlgebraOps): Added functions: sum, scale, axpy

--- src/main/scala/module1/homework/LinearAlgebraOps.scala
+++ src/main/scala/module1/homework/LinearAlgebraOps.scala
@@ -1,5 +1,19 @@
 package module1.homework
 
 object LinearAlgebraOps{
-  def sum(v1: Array[Int], v2: Array[Int]): Array[Int] = ???
+  def sum(v1: Array[Int], v2: Array[Int]): Array[Int] = {
+    if (v1.length != v2.length) {
+      throw new Exception("Operation is not supported")
+    }
+    var result: Array[Int] = Array()
+    for (item <- 0 until v1.length) {
+      result :+= v1(item) + v2(item)
+    }
+    result
+  }
+
+  def scale(a: Int, v: Array[Int]): Array[Int] = v.map(x => x * a)
+
+  def axpy(a: Int, v1: Array[Int], v2: Array[Int]) = sum(scale(a, v1), v2)
+
 }
