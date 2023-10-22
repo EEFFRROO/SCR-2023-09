abcac4d45875a7941fcd6d82e89b94af42046556
Ефремов Кирилл <kefremov@htc-cs.ru>
2023-10-22 17:17:23 +0400

    feat(future_collections): realized homework

--- src/main/scala/module1/homework/futures/task_futures_sequence.scala
+++ src/main/scala/module1/homework/futures/task_futures_sequence.scala
@@ -2,7 +2,8 @@ package module1.homework.futures
 
 import module1.homework.futures.HomeworksUtils.TaskSyntax
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success}
 
 object task_futures_sequence {
 
@@ -20,6 +21,26 @@ object task_futures_sequence {
    * @return асинхронную задачу с кортежом из двух списков
    */
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`" ()
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+
+    val promise = Promise[(List[A], List[Throwable])]()
+
+    def loop(list: List[Future[A]], result: (List[A], List[Throwable])): Unit =
+      list match {
+        case ::(head, tail) => head.onComplete {
+          case Success(v) => loop(tail, (result._1 :+ v, result._2))
+          case Failure(e) => loop(tail, (result._1, result._2 :+ e))
+        }
+        case Nil => promise.success(result)
+      }
+
+    loop(futures, (List.empty[A], List.empty[Throwable]))
+
+    promise.future
+
+//    sequence match {
+//      case future: Future[(List[A], List[Throwable])] => future
+//      case _: Unit => Future((List.empty[A], List.empty[Throwable]))
+//    }
+  }
 }
