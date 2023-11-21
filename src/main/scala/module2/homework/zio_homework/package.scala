8096e3c4573fb3ec89ba75529dcaf393291ad20e
kefremov <kefremov@htc-cs.ru>
2023-11-21 13:17:09 +0400

    feat(homework 7): realized homework

--- src/main/scala/module2/homework/zio_homework/package.scala
+++ src/main/scala/module2/homework/zio_homework/package.scala
@@ -1,17 +1,16 @@
 package module2.homework
 
 import module2.{effectConcurrency, zioConcurrency}
-import zio.{Has, Task, ULayer, ZIO, ZLayer}
+import zio.{Has, Task, ULayer, ZIO, ZLayer, clock, console, random}
 import zio.clock.{Clock, sleep}
 import zio.console._
 import zio.duration.durationInt
 import zio.macros.accessible
-
-//import scala.util.Random
 import zio.random._
 
 import java.io.IOException
 import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.MILLISECONDS
 import scala.io.StdIn
 import scala.language.postfixOps
 
@@ -21,30 +20,26 @@ package object zio_homework {
    * Используя сервисы Random и Console, напишите консольную ZIO программу которая будет предлагать пользователю угадать число от 1 до 3
    * и печатать в когнсоль угадал или нет. Подумайте, на какие наиболее простые эффекты ее можно декомпозировать.
    */
-
-
-
-  lazy val guessProgram: Task[Unit] = {
-    def guessEffect(rand: Int): ZIO[Any, Throwable, Unit] = ZIO.effect(StdIn.readLine().toInt).flatMap(num => ZIO.effect(
-      if (num == rand) {
-        println("Угадано")
-      } else {
-        println(s"Не угадано. Загаданное число $rand")
-      }
-    ))
+  lazy val guessProgram = {
     for {
-      randomized <- Random.Service.live.nextIntBetween(1, 4)
-      result <- ZIO.effect(println("Угадайте число от 1 до 3")) andThen guessEffect(randomized)
+      randomized <- random.nextIntBetween(1, 4)
+      _ <- ZIO.effect(println("Угадайте число от 1 до 3"))
+      result <- ZIO.effect(StdIn.readLine().toInt)
+      isSuccessful <- ZIO.effect(result == randomized)
+      _ <- ZIO.effect(if (isSuccessful) println("Угадано") else println("Не угадано"))
     } yield result
   }
 
   /**
    * 2. реализовать функцию doWhile (общего назначения), которая будет выполнять эффект до тех пор, пока его значение в условии не даст true
-   * 
+   *
    */
 
-  def doWhile[R, E, A](f: ZIO[R, E, A] => Boolean) = {
-
+  def doWhile[R, E, A](z: ZIO[R, E, A])(f: A => Boolean): ZIO[R, E, A] = {
+    for {
+      x <- z
+      _ <- if (f(x)) ZIO.unit else doWhile(z)(f)
+    } yield x
   }
 
 
@@ -59,15 +54,18 @@ package object zio_homework {
    * 3.1 Создайте эффект, который будет возвращать случайеым образом выбранное число от 0 до 10 спустя 1 секунду
    * Используйте сервис zio Random
    */
-  lazy val eff = {
-    ZIO.sleep(zio.duration.Duration.fromMillis(1000))
-    Random.Service.live.nextIntBetween(0, 11)
+  lazy val eff: ZIO[Random with Clock, Nothing, Int] = {
+    for {
+      _ <- ZIO.sleep(zio.duration.Duration.fromMillis(1000))
+      r <- random.nextIntBetween(0, 11)
+    } yield r
   }
 
   /**
    * 3.2 Создайте коллукцию из 10 выше описанных эффектов (eff)
    */
-  lazy val effects: ZIO[Any, Nothing, List[Int]] = ZIO.collectAll(List.fill(10)(eff))
+  lazy val effects: ZIO[Random with Clock, Nothing, List[Int]] = ZIO.collectAll(List.fill(10)(eff))
+  lazy val effectsPar: ZIO[Random with Clock, Nothing, List[Int]] = ZIO.collectAllPar(List.fill(10)(eff))
 
   
   /**
@@ -77,7 +75,11 @@ package object zio_homework {
    */
 
   lazy val app = {
-    zioConcurrency.printEffectRunningTime(effects)
+    effects.flatMap(list => ZIO.effect {
+      val sum = list.sum
+      println(sum)
+      sum
+    })
   }
 
 
@@ -85,13 +87,28 @@ package object zio_homework {
    * 3.4 Усовершенствуйте программу 4.3 так, чтобы минимизировать время ее выполнения
    */
 
-  lazy val appSpeedUp = ???
+  lazy val appSpeedUp = {
+    effectsPar.flatMap(list => ZIO.effect {
+      val sum = list.sum
+      println(sum)
+      sum
+    })
+  }
 
 
   /**
    * 4. Оформите ф-цию printEffectRunningTime разработанную на занятиях в отдельный сервис, так чтобы ее
    * молжно было использовать аналогично zio.console.putStrLn например
    */
+
+  def printEffectRunningTime[R, E, A](zio: ZIO[R, E, A]): ZIO[Console with Clock with R, Throwable, Unit] = {
+    for {
+      startTime <- clock.currentTime(MILLISECONDS)
+      _ <- zio.run
+      endTime <- clock.currentTime(MILLISECONDS)
+      zioEffect <- console.putStrLn((endTime - startTime) + " milliseconds")
+    } yield zioEffect
+  }
 
 
    /**
@@ -101,13 +118,24 @@ package object zio_homework {
      * 
      */
 
-  lazy val appWithTimeLogg = ???
+  lazy val appWithTimeLogg = {
+    printEffectRunningTime(app)
+  }
+
+  lazy val appSpeedUpWithTimeLogg = {
+    printEffectRunningTime(appSpeedUp)
+  }
 
   /**
     * 
     * Подготовьте его к запуску и затем запустите воспользовавшись ZioHomeWorkApp
     */
 
-  lazy val runApp = ???
+  lazy val runApp = {
+    for {
+      _ <- appSpeedUpWithTimeLogg
+      _ <- appWithTimeLogg
+    } yield ()
+  }
 
 }
